25859f3622dd522f3394dbe334df708572025607
nafanh <topkekoutof10@gmail.com>
2019-05-15 23:41:11 -0500

    Chapter 7 solutions to Programming exercistes

---
+++ Ch 7/Magic 8 ball.py
@@ -0,0 +1,9 @@
+import random
+def main():
+    infile = open('8_ball_responses.txt','r')
+    responses = infile.readlines()
+    for i in range(len(responses)):
+        responses[i] = responses[i].rstrip('\n')
+    question = input("Enter a question: ")
+    print(responses[random.randint(0,len(responses)-1)])
+main()
