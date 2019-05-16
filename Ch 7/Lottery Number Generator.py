b96979d6623b8277865550929d1e7fbe2096f4dd
nafanh <topkekoutof10@gmail.com>
2019-05-15 19:15:51 -0500

    Ch. 6

---
+++ Ch 7/Lottery Number Generator.py
@@ -0,0 +1,12 @@
+import random
+def generate_randoms():
+    random_list = []
+    for i in range(7):
+        random_list.append(random.randint(0,9))
+    return random_list
+
+def main():
+    random_num_list = generate_randoms()
+    for rand in random_num_list:
+        print(rand)
+main()
